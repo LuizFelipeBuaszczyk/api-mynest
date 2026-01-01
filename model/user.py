1b7f14abc465c1d0b3bd1fca5a662ef27fb37101
LuizFelipeBuaszczyk <luizfelipebuaszczyk@hotmail.com>
2025-12-31 23:30:24 -0300

    feat: Configuração inicial SQLAlchemy e Alembic, primeira migration para criação da tabela usuário

---
+++ model/user.py
@@ -0,0 +1,26 @@
+import uuid
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+
+from model import Base
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(
+        UUID(as_uuid=True), 
+        primary_key=True,
+        default=uuid.uuid4,
+        nullable=False,
+        )
+    
+    username = Column(
+        String(50),
+        unique=True,
+        nullable=False,
+    )
+
+    password = Column(
+        String(200),
+        nullable=False
+    )
